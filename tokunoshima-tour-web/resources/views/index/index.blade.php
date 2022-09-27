80db0c2257ffa839a61d24689a8fc5de6e444bbc
haruya46 <pazu70@gmail.com>
2022-09-27 23:12:13 +0900

    20220927

---
+++ tokunoshima-tour-web/resources/views/index/index.blade.php
@@ -0,0 +1,74 @@
+<x-app-layout>
+  <x-slot name="header">
+    <h1>home</h1>
+  </x-slot>
+
+    <div class="top-wrapper" >
+      <div class="container">
+        <div>
+          <h1>徳之島の魅力&癒やしツアー</h1>
+          <p>あなたの心は美しい それをとりもどそう</p>
+          <div class="btn-wrapper">
+            <a href="#" class="btn">口コミをみる</a>
+            <a href="#" class="btn">予約空き状況確認</a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="lesson-top">
+      <div class="lesson-wrapper">
+        <div class="container">
+          <div class="heading">
+            <h2>There is emotion<br> which remember your humor and kindness！</h2>
+          </div>
+          <div class="lessons">
+            <div class="lesson">
+              <div class="lesson-icon">
+                <h2>１日目</h2>
+                <img src="../../../storage/image/childInTheSea.jpg">
+              </div>
+              <p class="txt-contents">一日目は海です！！徳之島の海はどこの海にも負けないほどきれいです。海の中にはサンゴ礁に囲まれています。自然に囲まれて心を休ませましょう！<br>・１０時集合・海中リラックス
+                ・星空観測</p>
+            </div>
+            <div class="lesson">
+              <div class="lesson-icon">
+                <p>２日目</p>
+                <img src="../img/kimura.jpg">
+
+              </div>
+              <p class="txt-contents">二日目は、山登りです。山には奄美のクロウサギやイノシシに出会うことがまれにできます。植物もいろんな植物があるのでガイドさんが歩きながら紹介していきます。<br>・井之川岳登山
+                ・お昼は山頂で卵おにぎり
+                ・晩ご飯は子どもたちとカレーライスお料理</p>
+            </div>
+            <div class="lesson">
+              <div class="lesson-icon">
+                <p>３日目</p>
+                <img src="../img/nightSky1.jpg">
+              </div>
+              <p class="txt-contents">三日目は、夜景を満喫しましょう！！徳之島は、夜は暗いのでその分星がよく見えます!田舎ならではの夜景を楽しみましょう！！<br>・午前中自由
+                ・プラネタリウム
+                ・夕方から長距離散歩
+                ・人狼ゲーム</p>
+            </div>
+            <div class="lesson">
+              <div class="lesson-icon">
+                <p>４日目</p>
+                <img src="../img/walking02.jpg">
+              </div>
+              <p class="txt-contents">4日目は、最後に郷土料理を食べましょう！！現地でしか味わえないものを盛り合わせを満腹に食べましょう！！<br>・海中眺空
+                ・お昼は郷土料理盛り合わせ
+                ・13時解散</p>
+            </div>
+          </div>
+        </div>
+      </div>
+     </div>
+      <div class="message-wrapper">
+        <div class="container">
+          <div class="heading">
+            <h2>さぁあなたも心を癒しに行きませんか？ </h2>
+          </div>
+        <span class="btn message">さっそくログインして詳しく見たい！</span>
+      </div>
+    </div>
+</x-app-layout>
